c2d96583f4d23827f9e946966ffb940a0353e6f9
hesham <hesham.khaled@badrit.com>
2013-11-22 20:15:39 +0200

    table page

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,34 @@
+
+
+$(document).ready(function(){
+	
+	
+	
+  var progressbar = $('#progressbar'),  
+        max = progressbar.attr('max'),  
+        time = (1000/max)*5,      
+        value = progressbar.val();  
+  
+    var loading = function() {  
+        value += 1;  
+        addValue = progressbar.val(value);  
+          
+        $('.progress-value').html(value + '%');  
+  
+        if (value == max) {  
+            clearInterval(animate);                      
+        }  
+    };  
+  
+    var animate = setInterval(function() {  
+        loading();  
+    }, time);  
+
+function setCurrentState(x){
+	var currentState= $('progress').val();
+	return currentState;
+
+}
+
+})
+	 
